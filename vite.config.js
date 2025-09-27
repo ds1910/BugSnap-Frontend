37f7603c52866ca3481bab02eb065c8476081b57
ds1910 <dineshsharma7429@gmail.com>
2025-09-28 01:20:53 +0530

    some

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server: {
-    port: 5173
-  },
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    }
-  }
 })
 //hello
